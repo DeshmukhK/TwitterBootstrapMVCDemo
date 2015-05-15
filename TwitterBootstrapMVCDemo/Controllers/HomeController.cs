f51be8af4d598500afee4222f7af4b4cd5c37467
DeshmukhK <Ketan_Deshmukh@mastercard.com>
2015-05-15 15:43:02 +0530

    test

---
+++ TwitterBootstrapMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwitterBootstrapMVCDemo.Models;
+
+namespace TwitterBootstrapMVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var testViewModel = new TestModel
+            {
+                Amount = 95.00d,
+                Description = "This is test description",
+                Email = "Ketan.K.Deshmukh@gmail.com",
+                Gender = "Male",
+                Password = "Test",
+                RememberMe = true,
+                UserName = "Ketan Deshmukh"
+            };
+            return View(testViewModel);
+        }
+
+
+    }
+}
---
+++ TwitterBootstrapMVCDemo/Models/TestModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterBootstrapMVCDemo.Models
+{
+    public class TestModel
+    {
+        public Double Amount { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string Gender { get; set; }
+        public string Description { get; set; }
+
+        public string Email { get; set; }
+
+    }
+
+    public class Location
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class MyViewModel
+    {
+        public int CarsDriven { get; set; }
+        public List<Car> AllCars { get; set; }
+        public List<Color> Colors { get; set; }
+    }
+
+    public enum Color
+    {
+        Red = 1,
+        Green = 2,
+        Blue = 3,
+        Yellow = 4
+    }
+}
